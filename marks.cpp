7ecb1b1a8266d28020e8f07a57585e385fa47a48
SaiSridhar007 <55989634+SaiSridhar007@users.noreply.github.com>
2019-12-06 11:07:54 +0530

    Add files via upload

--- marks.cpp
+++ marks.cpp
@@ -4,13 +4,12 @@ main()
 	int m , p , c , tot ;
 	float avg;
 	printf("ENter the marks of Math");
-	printf("/n ENter the marks of Physics");
-	printf("/n Enter the marks of Chemistry");
+	printf("\n ENter the marks of Physics");
+	printf("\n Enter the marks of Chemistry \n");
 	scanf("%d%d%d",&m,&p,&c);
 	tot = m + p + c;
-	avg = tot/3;
-	printf("/n Total marks = %d",tot);
-	printf("/n AVERAGE Marks = %f", avg);
+	avg = (float)tot/3;
+	printf("\n Total marks = %d",tot);
+	printf("\n AVERAGE Marks = %f", avg);
 return 0;
-	
 }
