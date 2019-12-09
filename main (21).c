5671ee5e0d541e3d94100389e3f36b1e5b1b19de
SaiSridhar007 <55989634+SaiSridhar007@users.noreply.github.com>
2019-12-09 12:14:50 +0530

    Add files via upload

---
+++ main (21).c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b;
+    for(a=1;a<=10;a++)
+    {
+        for(b=1;b<=20;b++)
+        {
+            printf("\n %d X %d=%d",a,b,a*b);
+        }
+        printf("\n\n");
+    }
+    
+}
